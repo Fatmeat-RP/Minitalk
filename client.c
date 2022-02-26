3195e4f53f76fe1c1edcbc5da2ff444ea807d1f5
Alexandre Carle-margueritte <acarle-m@z3r11p5.42lyon.fr>
2022-02-26 17:06:43 +0100

    kasd

--- client.c
+++ client.c
@@ -8,44 +8,49 @@ static void	send_signal(int pid, char *string)
 	int	i;
 
 	i = 0;
-	while (string[i++ + 1])
+	while (string[i])
 	{
 		shift = 0;
-		while (shift < 7)
+		while (shift)
 		{
 			if ((string[i] >> shift) & 1)
-				kill(pid, SIGUSR2);
-			else
 				kill(pid, SIGUSR1);
-			shift++;
+			else
+				kill(pid, SIGUSR2);
+			shift--;
 			usleep(200);
 		}
+		i++;
 	}
 }
 
-static char	get_error(int ac, char **av, int pid)
+static int	get_error(int ac, char **av)
 {
+	int	pid;
+
 	if (ac != 3)
 	{
 		printf("Error, need two arguments, the pid then the string to send");
-		return (1);
+		return (-1);
 	}
-	pid = ft_atoi(av[1]);
-	if (!pid)
+	pid = atoi(av[1]);
+	if (pid < 1)
 	{
 		printf("Error, the pid is invalid. it need to be an int");
-		return (2);
+		return (-1);
 	}
-	return (0);
+	return (pid);
 }
 
 int	main(int ac, char **av)
 {
-	int	pid;
+	int		pid;
 	char	*string;
 
 	pid = 0;
-	if (get_error(ac, av, pid))
+	pid = get_error(ac, av);
+	printf("%d", pid);
+	if (pid == -1)
 		return (-1);
 	string = av[2];
 	send_signal(pid, string);
--- server.c
+++ server.c
@@ -2,25 +2,33 @@
 #include <stdio.h>
 #include <signal.h>
 
+	static char	c = 0;
+	static int	i = 0;
+
 static void	sig_handler(int sig)
 {
-	static char	c;
-	static int	i;
-
-	c += ((sig & 1) << i);
-	i++;
-	if (i == 8)
+	if (sig == SIGUSR1)
+//		c += (1 << i++);
+		write(1, "1", 1);
+	else if (sig == SIGUSR2)
+		write(1, "0", 1);
+//		i++;
+	if (i == 7)
 	{
 		write(1, &c, 1);
 		i = 0;
+		c = 0;
 	}
+	usleep(200);
 }
 
 int	main(void)
 {
 	printf("PID : %d\n", getpid());
-	signal (SIGUSR2, sig_handler);
 	signal (SIGUSR1, sig_handler);
+	signal (SIGUSR2, sig_handler);
 	while (1)
+	{
 		pause();
+	}
 }	
